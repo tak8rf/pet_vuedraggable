6f460f8cb9efcce9f41f4b8aad42a87e6d1d81be
Takumi Nagashima <emperte2008@gmail.com>
2022-08-15 16:00:34 +0900

    メール

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,17 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+  config.action_mailer.default_url_options = { host: 'hogehoge-fugafuga.herokuapp.com' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+}
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
